83adaa22bada175ea65adb4be026f9cffafe27c8
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2025-01-28 10:06:06 +0700

    try new

--- index.js
+++ index.js
@@ -1,51 +1,23 @@
 import express from "express";
-import { YtdlCore, toPipeableStream } from "@ybd-project/ytdl-core";
-
-const ytdl = new YtdlCore({
-  originalProxy: {
-    base: "https://lively-unit-fec6.nsfuad5.workers.dev/",
-
-    download: "https://lively-unit-fec6.nsfuad5.workers.dev/download",
-
-    urlQueryName: "url",
-  },
-  logDisplay: ["debug", "info", "success", "warning", "error"],
-
-  clients: [
-    "web",
-    "webCreator",
-    "webEmbedded",
-    "android",
-    "ios",
-    "tv",
-    "tvEmbedded",
-    "mweb",
-  ],
-});
+import { getData, filter, getHome } from "@hydralerne/youtube-api";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/stream/:url", (req, res) => {
-  const videoUrl = req.params.url; // Ambil URL video dari parameter query
+app.get("/stream/:url", async (req, res) => {
+  const videoId = req.params.url; // Ambil URL video dari parameter query
 
-  if (!videoUrl) {
-    return res.status(400).send("Video URL is required");
-  }
-  res.setHeader("Content-Type", "audio/mp3");
+  const formats = await getData(videoId);
+  const bestAudio = filter(formats, "bestaudio", {
+    minBitrate: 128000,
+    codec: "mp4a",
+  });
+  res.json(bestAudio);
+});
 
-  ytdl
-    .download(`https://www.youtube.com/watch?v=${videoUrl}`, {
-      filter: "audioonly",
-    })
-    .then((stream) => {
-      toPipeableStream(stream).pipe(res);
-    })
-    .catch((err) => {
-      res.json({
-        error: err.message,
-      });
-    });
+app.get("/explore", async (req, res) => {
+  const homeData = await getHome();
+  res.json(homeData);
 });
 
 app.listen(PORT, () => {
